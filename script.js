d0b0a56f61816f790bed7499deb05bfbbd848e3f
allazaoallado <allanv985@gmail.com>
2022-09-29 01:55:30 -0300

    ajuste no script

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var botao1 = window.document.getElementById('nota1')
-botao1.style.borderColor = '#f50505'
+// var botao1 = window.document.getElementById('nota1')
+// botao1.style.borderColor = '#f50505'
 
-var botao2 = window.document.getElementById('nota2')
-botao1.style.borderColor = '#f50505'
+// var botao2 = window.document.getElementById('nota2')
+// botao1.style.borderColor = '#f50505'
 
-var botao3 = window.document.getElementById('nota3')
-botao1.style.borderColor = '#f50505'
+// var botao3 = window.document.getElementById('nota3')
+// botao1.style.borderColor = '#f50505'

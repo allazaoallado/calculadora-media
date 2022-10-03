9444a8b521bb0ea047e08e3c87b6c60759802b2b
allazaoallado <allanv985@gmail.com>
2022-10-03 01:09:05 -0300

    refaturando o css

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ function somar() {
   .getElementById('nota2')
  var nota_3 = window.document
   .getElementById('nota3')
- var soma = (Number(nota_1.value) +
-   Number(nota_2.value) + Number(nota_3.value)
+ var soma = (Number(nota_1.value.replace(',','.')) +
+   Number(nota_2.value.replace(',','.')) + Number(nota_3.value.replace(',','.'))
    ) / 3
 
  var resposta = window.document
   .getElementById('resposta')
 
-if(nota_1.value < 0 || nota_1.value > 10 || isNaN(nota_1.value)){window.alert('A nota inserida no 1° trimestre é inválida')
+  var agora = new Date()
+  var hora = agora.getHours()// para puxar o horário atual
+  var saudacao = ''
+  if(hora > 12 && hora < 18){
+  saudacao ='Boa tarde'
+  }else if(hora >= 18 || hora < 6){
+  saudacao ='Boa noite'
+  }else{
+  saudacao = 'Bom dia'
+  }
+
+if(nota_1.value < 0 || nota_1.value > 10 || isNaN(nota_1.value.replace(',','.'))){window.alert('A nota inserida no 1° trimestre é inválida')
 resposta.innerHTML = ''
-}else if(nota_2.value < 0 || nota_2.value > 10 || isNaN(nota_2.value)){
+}else if(nota_2.value < 0 || nota_2.value > 10 || isNaN(nota_2.value.replace(',','.'))){
 window.alert('A nota inserida no 2° trimestre é inválida')
 resposta.innerHTML = ''
-}else if(nota_3.value < 0 || nota_3.value > 10 || isNaN(nota_3.value)){
+}else if(nota_3.value < 0 || nota_3.value > 10 || isNaN(nota_3.value.replace(',','.'))){
 window.alert('A nota inserida no 3° trimestre é inválida')
 resposta.innerHTML = ''
 }else{
-resposta.innerHTML = `A sua média é ${soma.toFixed(1).replace('.',',')}`
+resposta.innerHTML = `${saudacao}, sua média é ${soma.toFixed(1).replace('.',',')}.`
 }
 
 

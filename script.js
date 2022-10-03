44c8c81a10a4ed6449057ec8f5801e5a7f9fdd4c
Allan Cordeiro <105115166+allazaoallado@users.noreply.github.com>
2022-10-03 00:03:23 -0300

    refaturando o código

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-// var botao1 = window.document.getElementById('nota1')
-// botao1.style.borderColor = '#f50505'
+function somar() {
+ var nota_1 = window.document
+  .getElementById('nota1')
+ var nota_2 = window.document
+  .getElementById('nota2')
+ var nota_3 = window.document
+  .getElementById('nota3')
+ var soma = (Number(nota_1.value) +
+   Number(nota_2.value) + Number(nota_3.value)
+   ) / 3
 
-// var botao2 = window.document.getElementById('nota2')
-// botao1.style.borderColor = '#f50505'
+ var resposta = window.document
+  .getElementById('resposta')
 
-// var botao3 = window.document.getElementById('nota3')
-// botao1.style.borderColor = '#f50505'
+if(nota_1.value < 0 || nota_1.value > 10 || isNaN(nota_1.value)){window.alert('A nota inserida no 1° trimestre é inválida')
+resposta.innerHTML = ''
+}else if(nota_2.value < 0 || nota_2.value > 10 || isNaN(nota_2.value)){
+window.alert('A nota inserida no 2° trimestre é inválida')
+resposta.innerHTML = ''
+}else if(nota_3.value < 0 || nota_3.value > 10 || isNaN(nota_3.value)){
+window.alert('A nota inserida no 3° trimestre é inválida')
+resposta.innerHTML = ''
+}else{
+resposta.innerHTML = `A sua média é ${soma.toFixed(1).replace('.',',')}`
+}
+
+
+
+ 
+ 
+}
+
+
